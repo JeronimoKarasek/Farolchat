d109557e31bf83f2b4c82f02bc713a1f8bdf6527
Jeronimo <junior.karaseks@gmail.com>
2025-10-31 22:53:30 -0300

    feat: deploy-only static site for now (disable /api by removing functions); will re-enable after team runtime fix

---
+++ api_disabled/picpay/checkout.js
@@ -0,0 +1,2 @@
+// moved from api/picpay/checkout.js to disable functions during static deploy
+module.exports = {};
---
+++ api_disabled/picpay/webhook.js
@@ -0,0 +1,2 @@
+// moved from api/picpay/webhook.js to disable functions during static deploy
+module.exports = {};
